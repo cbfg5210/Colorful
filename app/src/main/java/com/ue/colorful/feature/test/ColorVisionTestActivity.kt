0424bfde24c97ba41c35b4c0f210b25382036b96
tom <120276969@qq.com>
2017-12-13 19:49:20 +0800

    色觉测试代码调整

--- app/src/main/java/com/ue/colorful/feature/test/ColorVisionTestActivity.kt
+++ app/src/main/java/com/ue/colorful/feature/test/ColorVisionTestActivity.kt
@@ -5,33 +5,29 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.squareup.picasso.Picasso
 import com.ue.colorful.R
 import kotlinx.android.synthetic.main.activity_color_vision_test.*
 import kotlinx.android.synthetic.main.fragment_test_img.view.*
 
 class ColorVisionTestActivity : AppCompatActivity() {
-    private lateinit var testAnswer: Array<String>
-    private lateinit var testImg: IntArray
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_vision_test)
 
-        testAnswer = resources.getStringArray(R.array.colorVisionAns)
-        testImg = intArrayOf(
-                R.raw.p01, R.raw.p02, R.raw.p03,
-                R.raw.p04, R.raw.p05, R.raw.p06,
-                R.raw.p07, R.raw.p08, R.raw.p09,
-                R.raw.p10, R.raw.p11, R.raw.p12,
-                R.raw.p13, R.raw.p14, R.raw.p15,
-                R.raw.p16, R.raw.p17, R.raw.p18,
-                R.raw.p19, R.raw.p20, R.raw.p21,
-                R.raw.p22, R.raw.p23, R.raw.p24)
-
+        val testAnswer = resources.getStringArray(R.array.colorVisionAns)
         val size = testAnswer.size
+        val states = SparseArray<Boolean>()
+
+        val testImgResTa = resources.obtainTypedArray(R.array.colorVisionImgs)
+        val testImgRes = Array(size, { i -> testImgResTa.getResourceId(i, 0) })
+        testImgResTa.recycle()
 
         //绑定自定义适配器
         vpTestPager.adapter = object : FragmentStatePagerAdapter(supportFragmentManager) {
@@ -40,7 +36,13 @@ class ColorVisionTestActivity : AppCompatActivity() {
             }
 
             override fun getItem(position: Int): Fragment {
-                return ColorVisionTestFragment.newInstance(position, testAnswer[position], testImg[position])
+                val fragment = ColorVisionTestFragment.newInstance(position, testAnswer[position], testImgRes[position], states[position, false])
+                fragment.setAnalyseListener(object : View.OnClickListener {
+                    override fun onClick(v: View?) {
+                        states.put(position, true)
+                    }
+                })
+                return fragment
             }
         }
 
@@ -63,20 +65,42 @@ class ColorVisionTestActivity : AppCompatActivity() {
         internal var pageNum: Int = 0
         internal var testAns: String = ""
         internal var testImg: Int = 0
+        internal var isVisible: Boolean = false
+        internal var onAnalyseListener: View.OnClickListener? = null
+
+        fun setAnalyseListener(onAnalyseListener: View.OnClickListener) {
+            this.onAnalyseListener = onAnalyseListener
+        }
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            if (arguments != null) {
-                pageNum = arguments.getInt(ARG_NUM)
-                testAns = arguments.getString(ARG_TEST_ANS)
-                testImg = arguments.getInt(ARG_TEST_IMG)
-            }
+
+            pageNum = arguments.getInt(ARG_NUM)
+            testAns = arguments.getString(ARG_TEST_ANS)
+            testImg = arguments.getInt(ARG_TEST_IMG)
+            isVisible = arguments.getBoolean(ARG_IS_VISIBLE)
         }
 
-        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-            val view = inflater!!.inflate(R.layout.fragment_test_img, container, false)
-            view.testImg.setImageResource(testImg)
-            view.ravAnswer.text = testAns
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            val view = inflater.inflate(R.layout.fragment_test_img, container, false)
+
+            Picasso.with(activity)
+                    .load(testImg)
+                    .into(view.testImg)
+
+            if (isVisible) {
+                view.tvTips.visibility = View.GONE
+                view.vgAnalysePanel.isSelected = true
+                (view.vsAnswer.inflate() as TextView).text = testAns
+            } else {
+                view.tvTips.setOnClickListener { v ->
+                    view.tvTips.visibility = View.GONE
+                    view.vgAnalysePanel.isSelected = true
+                    (view.vsAnswer.inflate() as TextView).text = testAns
+                    onAnalyseListener?.onClick(null)
+                }
+            }
+
             return view
         }
 
@@ -84,13 +108,15 @@ class ColorVisionTestActivity : AppCompatActivity() {
             private val ARG_NUM = "arg_num"
             private val ARG_TEST_ANS = "arg_test_ans"
             private val ARG_TEST_IMG = "arg_test_img"
+            private val ARG_IS_VISIBLE = "arg_is_visible"
 
-            fun newInstance(num: Int, testAns: String, testImg: Int): ColorVisionTestFragment {
+            fun newInstance(num: Int, testAns: String, testImg: Int, isVisible: Boolean): ColorVisionTestFragment {
                 val fragment = ColorVisionTestFragment()
                 val args = Bundle()
                 args.putInt(ARG_NUM, num)
                 args.putString(ARG_TEST_ANS, testAns)
                 args.putInt(ARG_TEST_IMG, testImg)
+                args.putBoolean(ARG_IS_VISIBLE, isVisible)
                 fragment.arguments = args
                 return fragment
             }
