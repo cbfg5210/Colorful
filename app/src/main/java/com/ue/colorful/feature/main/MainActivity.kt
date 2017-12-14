2807888a1a66cf6ec81788a88db9cc6c54527214
tom <120276969@qq.com>
2017-12-14 22:19:27 +0800

    主页显示色板

--- app/src/main/java/com/ue/colorful/feature/main/MainActivity.kt
+++ app/src/main/java/com/ue/colorful/feature/main/MainActivity.kt
@@ -2,13 +2,18 @@ package com.ue.colorful.feature.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.google.gson.reflect.TypeToken
 import com.ue.colorful.R
 import com.ue.colorful.constant.FunFlags
+import com.ue.colorful.constant.SPKeys
 import com.ue.colorful.model.ColorFunCategory
 import com.ue.colorful.model.ColorFunction
+import com.ue.colorful.util.GsonHolder
+import com.ue.colorful.util.SPUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -57,9 +62,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.menuPalette -> Toast.makeText(this, "palette", Toast.LENGTH_SHORT).show()
+            R.id.menuPalette -> showPalette()
             R.id.menuAbout -> Toast.makeText(this, "about", Toast.LENGTH_SHORT).show()
         }
         return true
+    }
+
+    private fun showPalette() {
+        val paletteColorsStr = SPUtils.getString(SPKeys.PALETTE_COLORS, "")
+        val paletteColors =
+                if (TextUtils.isEmpty(paletteColorsStr)) ArrayList<Int>()
+                else GsonHolder.gson.fromJson(paletteColorsStr, object : TypeToken<ArrayList<Int>>() {}.type)
+
+        if (paletteColors.size == 0) {
+            Toast.makeText(this, getString(R.string.no_palette_color), Toast.LENGTH_SHORT).show()
+            return
+        }
+        val dialog = ColorPaletteDialog.newInstance(paletteColors)
+        dialog.show(supportFragmentManager, "")
     }
 }
